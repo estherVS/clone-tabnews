fcf5b38b6d6c2d26ed45402b7a63f4d74d885d22
Esther <64289808+estherVS@users.noreply.github.com>
2024-08-26 00:15:30 +0000

    add pages folder

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return(
+        <div>
+            <h1> 
+                If you wish to make an apple pie from scratch, you must first invent the universe  
+            </h1>
+        </div>)
+
+}
+
+export default Home;
